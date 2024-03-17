c87521007925735f04db9f04b66ecf8c124c5957
Timo Reichl <thetredev@gmail.com>
2024-03-17 08:58:35 +0100

    scripts/create_packages: Update XBPS and OS packages before using xbps-src
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

--- scripts/create_packages.sh
+++ scripts/create_packages.sh
@@ -15,6 +15,13 @@ for xbps_pkg in $(ls -1 /workspace/packages); do
     sed -i "s/%XDEB_INSTALL_BINARY_SHA256%/${xbps_pkg_binary_checksum}/" srcpkgs/${xbps_pkg}/template
     sed -i "s/%XDEB_INSTALL_LICENSE_SHA256%/${xbps_pkg_license_checksum}/" srcpkgs/${xbps_pkg}/template
 
+    # lint the package sources
     xlint ${xbps_pkg} || exit 1
+
+    # update XBPS and OS packages
+    xbps-install -Syu xbps || exit 1
+    xbps-install -yu || exit 1
+
+    # build package
     ./xbps-src pkg -Q ${xbps_pkg} || exit 1
 done
